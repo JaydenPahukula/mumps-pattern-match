eb2d935b7a3060b59c2e20aeca38cc557dac3f42
JaydenPahukula <jayden.pahukula@gmail.com>
2025-09-24 18:25:51 -0500

    do no export PATCODES

--- lib/nfa/generate.d.ts
+++ lib/nfa/generate.d.ts
@@ -1,6 +1,3 @@
 import { AST } from '../ast/types.js';
 import { NFA } from './types.js';
 export declare function generateNFA(tree: AST): NFA;
-export declare const PATCODES: {
-    [key: string]: string | null;
-};

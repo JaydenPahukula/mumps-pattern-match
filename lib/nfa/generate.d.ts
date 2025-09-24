2e424a7da06a69092ba5fafd1ee53ac2af4db6a8
JaydenPahukula <jayden.pahukula@gmail.com>
2025-09-23 19:53:51 -0500

    simplify patcode lookup

--- lib/nfa/generate.d.ts
+++ lib/nfa/generate.d.ts
@@ -1,3 +1,6 @@
 import { AST } from "../ast/types.js";
 import { NFA } from "./types.js";
 export declare function generateNFA(tree: AST): NFA;
+export declare const PATCODES: {
+    [key: string]: string | null;
+};
